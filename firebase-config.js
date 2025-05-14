e76d1cd2ba013ceea9632f3a8e4a687ab70d2999
Royson Dsouza <149456436+RoysonDsz@users.noreply.github.com>
2025-05-14 13:05:27 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,13 +5,13 @@ import { getFirestore, collection, getDocs, getDoc, doc, setDoc } from 'https://
 import { getStorage, ref, uploadBytesResumable, getDownloadURL, listAll, deleteObject } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "process.env.FIREBASE_API_KEY",
+  authDomain: "process.env.FIREBASE_AUTH_DOMAIN",
+  projectId: "process.env.FIREBASE_PROJECT_ID",
+  storageBucket: "process.env.FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "process.env.FIREBASE_MESSAGING_SENDER_ID",
+  appId: "process.env.FIREBASE_APP_ID",
+  measurementId: "process.env.FIREBASE_MEASUREMENT_ID"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
