e361ed9f68db454296ce551dfe010a16707333ed
Royson Dsouza <149456436+RoysonDsz@users.noreply.github.com>
2025-05-14 13:25:37 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,19 @@
 // firebase-config.js
+require('dotenv').config();
+
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-app.js';
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js';
 import { getFirestore, collection, getDocs, getDoc, doc, setDoc } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL, listAll, deleteObject } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "process.env.FIREBASE_API_KEY",
-  authDomain: "process.env.FIREBASE_AUTH_DOMAIN",
-  projectId: "process.env.FIREBASE_PROJECT_ID",
-  storageBucket: "process.env.FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "process.env.FIREBASE_MESSAGING_SENDER_ID",
-  appId: "process.env.FIREBASE_APP_ID",
-  measurementId: "process.env.FIREBASE_MEASUREMENT_ID"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
